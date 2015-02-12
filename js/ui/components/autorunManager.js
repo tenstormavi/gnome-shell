48f12fe2ebe1152a7f750afff325a0a3b28adeb5
Florian Müllner <fmuellner@gnome.org>
2015-02-13 00:09:38 +0100

    Update notification style

--- js/ui/components/autorunManager.js
+++ js/ui/components/autorunManager.js
@@ -412,7 +412,7 @@ const AutorunNotification = new Lang.Class({
                                      x_fill: true,
                                      x_align: St.Align.START,
                                      button_mask: St.ButtonMask.ONE,
-                                     style_class: 'hotplug-notification-item' });
+                                     style_class: 'notification-button' });
 
         button.connect('clicked', Lang.bind(this, function() {
             startAppForMount(app, this._mount);
